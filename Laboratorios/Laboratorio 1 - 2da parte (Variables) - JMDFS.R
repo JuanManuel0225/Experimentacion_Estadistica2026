f719825b56660a7f4ce190eb520a118b34f19780
JuanManuel0225 <juan.delafuenteso@uanl.edu.mx>
2026-06-06 01:44:50 -0600

    Add files via upload
    2da parte del Laboratorio 1: Variables
    
    Identifique el tipo de variable (cualitativa o cuantitativa) para la lista de preguntas de una encuesta aplicada a estudiantes universitarios en una clase de estadística.

---
+++ Laboratorios/Laboratorio 1 - 2da parte (Variables) - JMDFS.R
@@ -0,0 +1,17 @@
+#2da parte del Laboratorio 1
+#Variables
+#Identifique el tipo de variable(cualitativa o cuantitativa) para la lista de
+#preguntas de una encuesta aplicada a estudiantes universitarios en una clase
+#de estadística:
+
+#Nombre de estudiante  --> Cualitativa
+#Fecha de nacimiento (p.Ej., 21/10/1995). --> Cuantitativa
+#Edad (en años) --> Cuantitativa
+#Dirección de casa (por ejemplo, 510-123-4567). --> Cualitativa
+#Número de teléfono --> Cualitativa
+#Área principal de estudio --> Cualitativa
+#Grado de año universitario: 1er año, 2do año, 3er año. --> Cualitativa
+#Puntaje en la prueba de mitad de período (basado en 100 puntos posibles) --> Cuantitativa
+#Calificación general: A,B,C,D,F. --> Cualtitativa
+#Tiempo (en minutos) para completar la prueba final de MCF 202. --> Cuantitativa
+#Número de hermanos. --> Cuantitativa 
