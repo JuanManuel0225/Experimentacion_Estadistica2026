dad0efde88dd7d02509501a56a21cbe5d9abcffa
JuanManuel0225 <juan.delafuenteso@uanl.edu.mx>
2026-06-05 22:33:22 -0600

    Add files via upload
    1er clase :
    Como crear una cuenta de GitHub y vincularla con nuestro RStudio.

---
+++ Scripts/1er clase - Experimentación.R
@@ -0,0 +1,13 @@
+# Experimentacion-2026-
+
+# Repositorio programa de maestría experimentación y métodos estadísticos 
+
+# Contenido del curso
+
+# **Sesion 1** 26/01/2026
+
+# + Crear cuenta de github
+
+# + Crear repositorio del curso
+
+# + Primera sincronización
